1652f70105597afaeae70bd7eb1980bb6f45f7c2
schrolan <schrolan000@gmail.com>
2023-12-02 13:13:47 -0600

    I filled out the tag-routes and updated the category routes. Everything is now working. DO not forget to npm run seed to seed the project.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -22,6 +22,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
     })
   
     if (!categoryData) {
